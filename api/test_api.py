78a6e2ddcc333d79a68c74d8525d7bc8f6b00b1b
joelethan <joelethan2.com>
2018-08-19 15:59:16 -0700

    push project to travis branch

--- api/test_api.py
+++ api/test_api.py
@@ -1,7 +1,34 @@
-# test_api.py
 import unittest
-import os
 import json
 from app import create_app
 
+class APITestCase(unittest.TestCase):
 
+    def setUp(self):
+        """Initialise the app and test variables"""
+        self.app = create_app(config_name="testing")
+        self.client=self.app.test_client
+
+
+    def test_post_answer(self):
+        post_resp = self.client().post('/questionz', data=json.dumps({
+            'qn_id':12,'qn':'what','aurthor':'j','answer':[{},{}]
+        }),content_type='application/json', )
+        data=json.loads(post_resp.get_data(as_text=True))
+        assert post_resp.status_code==200
+        assert data['qn_id'] == 12
+    
+    def test_post_an_answer(self):
+        post_resp = self.client().post('/questionz/122/answers', data=json.dumps({
+            'ans_id': 8,'answer': 'Yes!','aurthor': 'jo','votedown':5,'voteup': 7
+            }),content_type='application/json', )
+        data=json.loads(post_resp.get_data(as_text=True))
+        assert post_resp.status_code == 200
+        assert data['ans_id']==8
+
+
+
+    
+
+if __name__=="__main__":
+    unittest.main()
