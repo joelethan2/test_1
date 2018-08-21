52fdb43939f17795c5ff4e753b5886e883ff3150
joelethan <joelethan2.com>
2018-08-21 14:28:01 -0700

    more tests added to test_api.py

--- api/test_api.py
+++ api/test_api.py
@@ -1,5 +1,5 @@
 import unittest
-import json
+from flask import json
 from app import create_app
 
 class APITestCase(unittest.TestCase):
@@ -9,25 +9,63 @@ class APITestCase(unittest.TestCase):
         self.app = create_app(config_name="testing")
         self.client=self.app.test_client
 
+        self.test_qn={
+                    'qn_id':12,
+                    'qn':'what',
+                    'aurthor':'j',
+                    'answer':[{},{}]
+                    }
+        self.test_ans={
+                    'ans_id': 8,
+                    'answer': 'Yes!',
+                    'aurthor': '   jo',
+                    'votedown':5,'voteup': 7
+                    }
 
-    def test_post_answer(self):
-        post_resp = self.client().post('/questionz', data=json.dumps({
-            'qn_id':12,'qn':'what','aurthor':'j','answer':[{},{}]
-        }),content_type='application/json', )
+
+
+    def test_questeion_has_id(self):
+        post_resp = self.client().post('/questionz', 
+                    data=json.dumps(self.test_qn),content_type='application/json')
         data=json.loads(post_resp.get_data(as_text=True))
-        assert post_resp.status_code==200
-        assert data['qn_id'] == 12
+        self.assertEqual(post_resp.status_code, 200)
+        self.assertEqual(data['qn_id'], 12)
+
+
+    def test_question_has_content(self):
+        post_resp = self.client().post('/questionz', 
+                    data=json.dumps(self.test_qn),content_type='application/json')
+        data=json.loads(post_resp.get_data(as_text=True))
+        self.assertEqual(post_resp.status_code, 200)
+        self.assertNotEqual(data['qn'].strip(), '')
     
-    def test_post_an_answer(self):
-        post_resp = self.client().post('/questionz/122/answers', data=json.dumps({
-            'ans_id': 8,'answer': 'Yes!','aurthor': 'jo','votedown':5,'voteup': 7
-            }),content_type='application/json', )
+    def test_question_has_aurthor(self):
+        post_resp = self.client().post('/questionz', 
+                    data=json.dumps(self.test_qn),content_type='application/json')
+        data=json.loads(post_resp.get_data(as_text=True))
+        self.assertEqual(post_resp.status_code, 200)
+        self.assertNotEqual(data['aurthor'].strip(), '')
+    
+
+    def test_answer_has_id(self):
+        post_resp = self.client().post('/questionz/122/answers', data=json.dumps(self.test_ans),content_type='application/json')
         data=json.loads(post_resp.get_data(as_text=True))
         assert post_resp.status_code == 200
         assert data['ans_id']==8
 
 
+    def test_answer_has_content(self):
+        post_resp = self.client().post('/questionz/122/answers', data=json.dumps(self.test_ans),content_type='application/json')
+        data=json.loads(post_resp.get_data(as_text=True))
+        self.assertEqual(post_resp.status_code, 200)
+        self.assertNotEqual(data['answer'].strip(), '')
 
+
+    def test_answer_has_aurthor(self):
+        post_resp = self.client().post('/questionz/122/answers', data=json.dumps(self.test_ans),content_type='application/json')
+        data=json.loads(post_resp.get_data(as_text=True))
+        self.assertEqual(post_resp.status_code, 200)
+        self.assertNotEqual(data['aurthor'].strip(), '')
     
 
 if __name__=="__main__":
